16b37280f45c21c1350bdc15734ae5f827ed492b
rhedin <cubsno1@gmail.com>
2017-08-20 21:49:30 +0000

    Now have client and server communicating.

--- src/pickGame.js
+++ src/pickGame.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 class PickGame extends Component {
+    constructor(props) {
+        super(props);
+        this.loadGameFile = this.loadGameFile.bind(this);
+    }
+    
     render() {
         return (
             <div>
@@ -24,6 +29,7 @@ class PickGame extends Component {
                 load a file that contains a new game<br />
                 <input type="file" className="button" id="getGameFile" style={ {display:'none'} } onChange={this.loadGameFile} />
                 <label htmlFor="getGameFile">Choose file</label>
+                {/* This label badly needs styling. */}
             </div>
         );
     }
@@ -33,10 +39,32 @@ class PickGame extends Component {
         const reader = new FileReader();
         reader.onload = (e) => {
             console.log(e.target.result);
+            this.sendGameToServer(e.target.result);
         };
         reader.readAsText(file);
-        // event.target.value = file.name + ' loaded';
         event.target.value = '';
+    }
+    
+    sendGameToServer(fileText) {
+        fetch('https://progwar-server-rhedin.c9users.io/gameToServer', {
+        // fetch('https://game-rhedin.c9users.io/sendInfo', {
+            method: 'POST',
+        })
+        .then(resp => {
+            if (resp.ok) {
+                return resp.json();
+            } else {
+                return Promise.reject({
+                    ok: resp.ok,
+                    status: resp.status,
+                    statusText: resp.statusText,
+                });
+            }
+        })
+        .catch(error => {
+            console.log('function: sendGameToServer  error: fetch failed');
+            console.log(error);
+        });
     }
 }
 
